dec88411df71c93e454557dbc8f2d3bd1010ca2c
abhishek <abhishek.k@loconav.com>
2020-02-01 10:31:13 +0530

    buy sell stocks

---
+++ src/Prog35.java
@@ -0,0 +1,23 @@
+public class Prog35 extends BaseTestClass {
+
+    public static void main(String... args) {
+        print("\nmaxProfit : " + maxProfit(new int[]{}));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int minNow = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minNow) {
+                minNow = prices[i];
+                continue;
+            }
+            int localMax = prices[i] - minNow;
+            if (localMax > maxProfit) maxProfit = localMax;
+        }
+
+        return maxProfit;
+    }
+}
